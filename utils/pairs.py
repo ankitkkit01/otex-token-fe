046171cae4b2ba5a8abdca2c55441d1ab9480028
ankitkkit01 <akhimanshya@gmail.com>
2025-07-02 19:23:26 +0530

    Add files via upload

---
+++ utils/pairs.py
@@ -0,0 +1,42 @@
+# utils/pairs.py
+
+pair_buttons = {
+    "USD/INR": "🇺🇸/🇮🇳 USD/INR",
+    "USD/PKR": "🇺🇸/🇵🇰 USD/PKR",
+    "USD/IDR": "🇺🇸/🇮🇩 USD/IDR",
+    "USD/MXN": "🇺🇸/🇲🇽 USD/MXN",
+    "USD/BRL": "🇺🇸/🇧🇷 USD/BRL",
+    "USD/ZAR": "🇺🇸/🇿🇦 USD/ZAR",
+    "USD/TRY": "🇺🇸/🇹🇷 USD/TRY",
+    "USD/COP": "🇺🇸/🇨🇴 USD/COP",
+    "USD/ARS": "🇺🇸/🇦🇷 USD/ARS",
+    "USD/DZD": "🇺🇸/🇩🇿 USD/DZD",
+    "NZD/CAD": "🇳🇿/🇨🇦 NZD/CAD",
+    "CAD/CHF": "🇨🇦/🇨🇭 CAD/CHF",
+    "USD/PHP": "🇺🇸/🇵🇭 USD/PHP",
+    "NZD/JPY": "🇳🇿/🇯🇵 NZD/JPY",
+    "USD/BDT": "🇺🇸/🇧🇩 USD/BDT",
+    "EUR/NZD": "🇪🇺/🇳🇿 EUR/NZD",
+    "GBP/NZD": "🇬🇧/🇳🇿 GBP/NZD",
+    "NZD/USD": "🇳🇿/🇺🇸 NZD/USD",
+    "USD/NGN": "🇺🇸/🇳🇬 USD/NGN",
+    "AUD/NZD": "🇦🇺/🇳🇿 AUD/NZD",
+    "GBP/CHF": "🇬🇧/🇨🇭 GBP/CHF",
+    "AUD/CHF": "🇦🇺/🇨🇭 AUD/CHF",
+
+    # Crypto
+    "BTC/USD": "🪙/🇺🇸 BTC/USD",
+    "ETH/USD": "🪙/🇺🇸 ETH/USD",
+    "XRP": "🪙 XRP",
+    "LTC": "🪙 LTC",
+    "DOGE": "🐶 DOGE",
+    "SOL": "🟠 SOL",
+    "SHIBA": "🐕 SHIBA",
+    "TRUMP": "🧑‍🦳 TRUMP",
+    "TON": "💠 TON",
+
+    # Commodities
+    "GOLD": "🟡 GOLD",
+    "SILVER": "⚪ SILVER",
+    "CRUDEOIL": "🛢️ CRUDEOIL"
+}
